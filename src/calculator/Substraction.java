4126a58644de5061dc098130c6799d2d0f2a89d4
Rashid Hussainn <7465002896.R@gmail.com>
2023-09-04 10:42:47 +0530

    substraction feature added

---
+++ src/calculator/Substraction.java
@@ -0,0 +1,17 @@
+package calculator;
+
+public class Substraction {
+
+	public static void main(String[] args) {
+
+
+		int x=90;
+		int y=85;
+		int sub=x-y;
+		
+		System.out.println(sub);
+		
+
+	}
+
+}
